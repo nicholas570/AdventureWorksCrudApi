605c000ef0f98ef6d8e36d19eeba5ebaeb9baef8
Nicolas Mercier <nmercier@d-edge.com>
2026-07-02 17:22:22 +0200

    feat: add sql queries logs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,50 @@
 using AdventureWorksCrudApi.Data;
 using AdventureWorksCrudApi.Endpoints;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("AdventureWorks");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+try
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    var connectionString = builder.Configuration.GetConnectionString("AdventureWorks");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+        if (builder.Environment.IsDevelopment())
+        {
+            options.EnableSensitiveDataLogging();
+        }
+    });
+
+    builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.MapProductEndpoints();
+
+    app.MapGet("/", () => "AdventureWorks CRUD API. See /api/products");
+
+    app.Run();
 }
-
-app.MapProductEndpoints();
-
-app.MapGet("/", () => "AdventureWorks CRUD API. See /api/products");
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Application stopped because of an exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
